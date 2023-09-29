75019e6e5ca362aabf9fd90133ce995ea75bbeab
Santiago Vallejo <santiagovallejoc@gmail.com>
2023-09-29 17:31:21 -0600

    project init

---
+++ analytics/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from analytics.users.models import User
+from analytics.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
